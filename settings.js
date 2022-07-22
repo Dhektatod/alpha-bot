3bd274018c8be2fa59397147c76eb5897a5ec2d0
Dhektatod <109351215+Dhektatod@users.noreply.github.com>
2022-07-22 12:59:56 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,7 +24,7 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
+global.botname = "Dhekta-bot-Mdོ" //namabot kalian
 global.ownername= "ᴹᴿ᭄ DhektatodOfcོ ×፝֟͜×" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
@@ -40,7 +40,7 @@ global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder i
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh dektatod Ofc' //sticker wm ganti nama kalian
+global.author = 'Di Buat Oleh alpha-md' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 
